fc69fd52854e08859a2ec71f25eac67e25ce7497
VictorLeite <victor.leite@gmail.com>
2017-07-23 23:31:58 -0300

    Implementando Downloader de Iso

--- PenDriveMaker/Form1.Designer.vb
+++ PenDriveMaker/Form1.Designer.vb
@@ -22,64 +22,215 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.btnStartDownload = New System.Windows.Forms.Button()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnDownload = New System.Windows.Forms.Button()
         Me.progressBar = New System.Windows.Forms.ProgressBar()
         Me.progressBarLabel = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.comboBoxVersions = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tabControl1 = New System.Windows.Forms.TabControl()
+        Me.downloadTab = New System.Windows.Forms.TabPage()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.downloadingLabel = New System.Windows.Forms.Label()
+        Me.makePenDriveTab = New System.Windows.Forms.TabPage()
+        Me.noConnectionLabel = New System.Windows.Forms.Label()
+        Me.isoFolderGroup = New System.Windows.Forms.GroupBox()
+        Me.isoFolderName = New System.Windows.Forms.TextBox()
+        Me.seachIsoFolderBtn = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.tabControl1.SuspendLayout()
+        Me.downloadTab.SuspendLayout()
+        Me.isoFolderGroup.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'btnStartDownload
+        'btnDownload
         '
-        Me.btnStartDownload.Location = New System.Drawing.Point(146, 251)
-        Me.btnStartDownload.Name = "btnStartDownload"
-        Me.btnStartDownload.Size = New System.Drawing.Size(75, 23)
-        Me.btnStartDownload.TabIndex = 0
-        Me.btnStartDownload.Text = "Download"
-        Me.btnStartDownload.UseVisualStyleBackColor = True
+        Me.btnDownload.BackColor = System.Drawing.Color.Transparent
+        Me.btnDownload.Location = New System.Drawing.Point(240, 109)
+        Me.btnDownload.Name = "btnDownload"
+        Me.btnDownload.Size = New System.Drawing.Size(75, 23)
+        Me.btnDownload.TabIndex = 0
+        Me.btnDownload.Text = "Download"
+        Me.btnDownload.UseVisualStyleBackColor = False
         '
         'progressBar
         '
-        Me.progressBar.Location = New System.Drawing.Point(15, 31)
+        Me.progressBar.Location = New System.Drawing.Point(17, 37)
         Me.progressBar.Name = "progressBar"
-        Me.progressBar.Size = New System.Drawing.Size(339, 23)
+        Me.progressBar.Size = New System.Drawing.Size(298, 12)
         Me.progressBar.TabIndex = 1
         Me.progressBar.Visible = False
         '
         'progressBarLabel
         '
         Me.progressBarLabel.AutoSize = True
-        Me.progressBarLabel.Location = New System.Drawing.Point(12, 15)
+        Me.progressBarLabel.Location = New System.Drawing.Point(15, 21)
         Me.progressBarLabel.Name = "progressBarLabel"
         Me.progressBarLabel.Size = New System.Drawing.Size(0, 13)
         Me.progressBarLabel.TabIndex = 2
+        Me.progressBarLabel.Visible = False
         '
-        'Button1
+        'comboBoxVersions
         '
-        Me.Button1.Location = New System.Drawing.Point(279, 71)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 3
-        Me.Button1.Text = "LoadCombo"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.comboBoxVersions.FormattingEnabled = True
+        Me.comboBoxVersions.Location = New System.Drawing.Point(17, 82)
+        Me.comboBoxVersions.Name = "comboBoxVersions"
+        Me.comboBoxVersions.Size = New System.Drawing.Size(298, 21)
+        Me.comboBoxVersions.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(14, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Educatux Versions"
+        '
+        'tabControl1
+        '
+        Me.tabControl1.Controls.Add(Me.downloadTab)
+        Me.tabControl1.Controls.Add(Me.makePenDriveTab)
+        Me.tabControl1.Location = New System.Drawing.Point(12, 221)
+        Me.tabControl1.Name = "tabControl1"
+        Me.tabControl1.SelectedIndex = 0
+        Me.tabControl1.Size = New System.Drawing.Size(342, 181)
+        Me.tabControl1.TabIndex = 6
+        '
+        'downloadTab
+        '
+        Me.downloadTab.Controls.Add(Me.btnCancel)
+        Me.downloadTab.Controls.Add(Me.downloadingLabel)
+        Me.downloadTab.Controls.Add(Me.Label1)
+        Me.downloadTab.Controls.Add(Me.comboBoxVersions)
+        Me.downloadTab.Controls.Add(Me.btnDownload)
+        Me.downloadTab.Controls.Add(Me.progressBarLabel)
+        Me.downloadTab.Controls.Add(Me.progressBar)
+        Me.downloadTab.Location = New System.Drawing.Point(4, 22)
+        Me.downloadTab.Name = "downloadTab"
+        Me.downloadTab.Padding = New System.Windows.Forms.Padding(3)
+        Me.downloadTab.Size = New System.Drawing.Size(334, 155)
+        Me.downloadTab.TabIndex = 0
+        Me.downloadTab.Text = "Download"
+        Me.downloadTab.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.BackColor = System.Drawing.Color.Transparent
+        Me.btnCancel.Location = New System.Drawing.Point(159, 109)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 7
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = False
+        '
+        'downloadingLabel
+        '
+        Me.downloadingLabel.AutoSize = True
+        Me.downloadingLabel.Location = New System.Drawing.Point(237, 21)
+        Me.downloadingLabel.Name = "downloadingLabel"
+        Me.downloadingLabel.Size = New System.Drawing.Size(78, 13)
+        Me.downloadingLabel.TabIndex = 6
+        Me.downloadingLabel.Text = "Downloading..."
+        Me.downloadingLabel.TextAlign = System.Drawing.ContentAlignment.TopRight
+        Me.downloadingLabel.Visible = False
+        '
+        'makePenDriveTab
+        '
+        Me.makePenDriveTab.Location = New System.Drawing.Point(4, 22)
+        Me.makePenDriveTab.Name = "makePenDriveTab"
+        Me.makePenDriveTab.Padding = New System.Windows.Forms.Padding(3)
+        Me.makePenDriveTab.Size = New System.Drawing.Size(334, 255)
+        Me.makePenDriveTab.TabIndex = 1
+        Me.makePenDriveTab.Text = "Make PenDrive"
+        Me.makePenDriveTab.UseVisualStyleBackColor = True
+        '
+        'noConnectionLabel
+        '
+        Me.noConnectionLabel.AutoSize = True
+        Me.noConnectionLabel.Location = New System.Drawing.Point(37, 205)
+        Me.noConnectionLabel.Name = "noConnectionLabel"
+        Me.noConnectionLabel.Size = New System.Drawing.Size(293, 13)
+        Me.noConnectionLabel.TabIndex = 7
+        Me.noConnectionLabel.Text = "No connection. Check the internet connection and try again."
+        Me.noConnectionLabel.Visible = False
+        '
+        'isoFolderGroup
+        '
+        Me.isoFolderGroup.Controls.Add(Me.isoFolderName)
+        Me.isoFolderGroup.Controls.Add(Me.seachIsoFolderBtn)
+        Me.isoFolderGroup.Location = New System.Drawing.Point(12, 418)
+        Me.isoFolderGroup.Name = "isoFolderGroup"
+        Me.isoFolderGroup.Size = New System.Drawing.Size(332, 57)
+        Me.isoFolderGroup.TabIndex = 8
+        Me.isoFolderGroup.TabStop = False
+        Me.isoFolderGroup.Text = "Iso Folder"
+        '
+        'isoFolderName
+        '
+        Me.isoFolderName.Enabled = False
+        Me.isoFolderName.Location = New System.Drawing.Point(21, 21)
+        Me.isoFolderName.Name = "isoFolderName"
+        Me.isoFolderName.Size = New System.Drawing.Size(203, 20)
+        Me.isoFolderName.TabIndex = 1
+        Me.isoFolderName.Text = "Select a folder for iso"
+        '
+        'seachIsoFolderBtn
+        '
+        Me.seachIsoFolderBtn.Location = New System.Drawing.Point(230, 18)
+        Me.seachIsoFolderBtn.Name = "seachIsoFolderBtn"
+        Me.seachIsoFolderBtn.Size = New System.Drawing.Size(88, 23)
+        Me.seachIsoFolderBtn.TabIndex = 0
+        Me.seachIsoFolderBtn.Text = "Search Folder"
+        Me.seachIsoFolderBtn.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(338, 190)
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(366, 358)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.progressBarLabel)
-        Me.Controls.Add(Me.progressBar)
-        Me.Controls.Add(Me.btnStartDownload)
+        Me.ClientSize = New System.Drawing.Size(364, 487)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.isoFolderGroup)
+        Me.Controls.Add(Me.noConnectionLabel)
+        Me.Controls.Add(Me.tabControl1)
         Me.Name = "Form1"
         Me.Text = "Educatux Magic"
+        Me.tabControl1.ResumeLayout(False)
+        Me.downloadTab.ResumeLayout(False)
+        Me.downloadTab.PerformLayout()
+        Me.isoFolderGroup.ResumeLayout(False)
+        Me.isoFolderGroup.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents btnStartDownload As Button
+    Friend WithEvents btnDownload As Button
     Friend WithEvents progressBar As ProgressBar
     Friend WithEvents progressBarLabel As Label
-    Friend WithEvents Button1 As Button
+    Friend WithEvents comboBoxVersions As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tabControl1 As TabControl
+    Friend WithEvents downloadTab As TabPage
+    Friend WithEvents makePenDriveTab As TabPage
+    Friend WithEvents downloadingLabel As Label
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents noConnectionLabel As Label
+    Friend WithEvents isoFolderGroup As GroupBox
+    Friend WithEvents isoFolderName As TextBox
+    Friend WithEvents seachIsoFolderBtn As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- PenDriveMaker/Form1.vb
+++ PenDriveMaker/Form1.vb
@@ -2,49 +2,132 @@
 Imports Newtonsoft.Json
 
 Public Class Form1
-    Dim WithEvents WC As New WebClient
+    Dim WithEvents client As New WebClient
+    Dim versionList As List(Of Version)
+    Dim versionSelected As Version
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) _
-    Handles btnStartDownload.Click
-        Dim client As WebClient = New WebClient
-        AddHandler client.DownloadProgressChanged, AddressOf client_ProgressChanged
-        AddHandler client.DownloadFileCompleted, AddressOf client_DownloadCompleted
-        client.DownloadFileAsync(New Uri("https://downloads.sourceforge.net/project/educatux/Debian/stretch/prod/educatux-debian9-cinnamon-i386.iso?r=https%3A%2F%2Fwww.educatux.com.br%2Fpromo%2Fdownload.php&ts=1500777543&use_mirror=ufpr"), "C:\tmp\educatux.iso")
-        btnStartDownload.Text = "Download in Progress"
-        btnStartDownload.Enabled = False
-        progressBar.Visible = True
+    Handles btnDownload.Click
+
+        If versionSelected IsNot Nothing Then
+
+            client = New WebClient
+            AddHandler client.DownloadProgressChanged, AddressOf client_ProgressChanged
+            AddHandler client.DownloadFileCompleted, AddressOf client_DownloadCompleted
+
+            Dim fileName As String = versionSelected.name.Replace(" ", "") & "_" & versionSelected.version & "_" & versionSelected.size & ".iso"
+            client.DownloadFileAsync(New Uri(versionSelected.iso), isoFolderName.Text & "\" & fileName)
+
+            btnCancel.Enabled = True
+            btnDownload.Enabled = False
+
+            progressBar.Visible = True
+            progressBarLabel.Visible = True
+            downloadingLabel.Visible = True
+            downloadingLabel.Text = "Downloading..."
+
+        Else
+
+            MessageBox.Show("Please, choose one version of Educatux")
+
+        End If
+
     End Sub
+
     Private Sub client_ProgressChanged(ByVal sender As Object, ByVal e As DownloadProgressChangedEventArgs)
+
         Dim bytesIn As Double = Double.Parse(e.BytesReceived.ToString())
         Dim totalBytes As Double = Double.Parse(e.TotalBytesToReceive.ToString())
         Dim percentage As Double = bytesIn / totalBytes * 100
         progressBarLabel.Text = Util.FormatBytes(bytesIn) & "/" & Util.FormatBytes(totalBytes)
-
         progressBar.Value = Integer.Parse(Math.Truncate(percentage).ToString())
+
     End Sub
+
     Private Sub client_DownloadCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs)
-        MessageBox.Show("Download Complete")
-        btnStartDownload.Text = "Start Download"
-        btnStartDownload.Enabled = True
+
+        btnDownload.Enabled = True
+        btnDownload.Text = "Download Complete"
+
     End Sub
 
-    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+
+    Private Sub LoadVersionCombo()
+
         Dim jsonString As String
-        Dim versions As List(Of Version)
-
         jsonString = ApiRest.GetEducatuxVersions()
-        versions = JsonConvert.DeserializeObject(Of List(Of Version))(jsonString)
-
-        For Each version As Version In versions
-            MessageBox.Show(version.version)
+        Me.versionList = JsonConvert.DeserializeObject(Of List(Of Version))(jsonString)
+        comboBoxVersions.Items.Clear()
+        For Each version As Version In Me.versionList
+            comboBoxVersions.Items.Add(version.name & " - " & version.version & " [ " & version.size & " ]")
         Next
+        comboBoxVersions.Text = "Select..."
+
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        If Not Util.CheckForInternetConnection Then
+
+            btnCancel.Visible = False
+            btnDownload.Visible = False
+            tabControl1.Visible = False
+            isoFolderGroup.Visible = False
+            noConnectionLabel.Visible = True
+
+        Else
+
+            btnCancel.Enabled = False
+            btnDownload.Enabled = True
+            LoadVersionCombo()
+            isoFolderName.Text = "C:\tmp"
+
+
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles btnCancel.Click
+
+        client.CancelAsync()
+        progressBar.Visible = False
+        progressBarLabel.Visible = False
+        downloadingLabel.Visible = False
+        progressBarLabel.Text = ""
+        downloadingLabel.Text = ""
+        btnCancel.Enabled = False
+        btnDownload.Enabled = True
+
+    End Sub
+
+    Private Sub comboBoxVersions_SelectedIndexChanged(sender As Object, e As EventArgs) Handles comboBoxVersions.SelectedIndexChanged
+
+        Me.versionSelected = Me.versionList(comboBoxVersions.SelectedIndex)
+
+    End Sub
+
+    Private Sub seachIsoFolderBtn_Click(sender As Object, e As EventArgs) Handles seachIsoFolderBtn.Click
+
+        Dim result As DialogResult = FolderBrowserDialog1.ShowDialog()
+
+        If (result = DialogResult.OK) Then
+
+            isoFolderName.Text = FolderBrowserDialog1.SelectedPath
+
+        End If
+
+
+    End Sub
+
 End Class
 
 
 Public Class Version
+
     Public Property name As String
     Public Property version As String
     Public Property checksum As String
     Public Property iso As String
+    Public Property size As String
+
 End Class
